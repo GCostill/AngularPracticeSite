91ad5744bb359092a48071dbdde680b7976a109c
Gabriella Costilla-Guzman <gabriella.costilla-guzman@kineticadvantage.com>
2022-12-14 11:01:47 -0500

    initial commit - starting html page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularPracticeSite';
+  title: string = 'Task Tracker';
 }
