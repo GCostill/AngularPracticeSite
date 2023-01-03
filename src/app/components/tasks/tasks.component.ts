e973a1faec2e85048599713eced3d4248cac18c1
Gabriella Costilla-Guzman <gabriella.costilla-guzman@kineticadvantage.com>
2023-01-03 09:53:03 -0500

    generated task-item compontent

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//This Task is the interface
+import {Task} from '../../Task';
+//This TASKS is the mocked data
+import {TASKS} from '../../mock-tasks';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
+
+  tasks: Task[] = TASKS;
+
+  constructor() {}
+
+  ngOnInit(): void{}
 
 }
