5a526685e88fd8cc39d7ca42d18b58833ade03e7
Gabriella Costilla-Guzman <gabriella.costilla-guzman@kineticadvantage.com>
2023-01-16 11:10:12 -0500

    app module and task service use httpclient

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,19 +1,18 @@
 import { Injectable } from '@angular/core';
-import{Observable, of} from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import{Observable} from 'rxjs';
 //This Task is the interface
 import {Task} from '../Task';
-//This TASKS is the mocked data
-import {TASKS} from '../mock-tasks';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
